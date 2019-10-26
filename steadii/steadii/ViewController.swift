acfc3e9e4e78590dfddb9a9c6741e9d4779bc1a9
Kay Arellano <karellan@sc-2300-46.labs.surrey.sfu.ca>
2019-10-26 16:23:08 -0700

    Initial commit

---
+++ steadii/steadii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  steadii
+//
+//  Created by Kay Arellano on 2019-10-26.
+//  Copyright © 2019 Kay Arellano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
