1753d1f28a439f4f2fc096e9cde2ba707079eda8
ckeilbar <chris_keilbart@sfu.ca>
2019-10-30 16:06:33 -0700

    Revert "Rescaled stuff, plane doesn't bounce off of sides, gravity currently off for debug purposes"
    This reverts commit f9cca7d5e3d9338ae53ef176680c7fb5ed5b742b

--- Steadii/GameScene.swift
+++ Steadii/GameScene.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2019 ii Studio. All rights reserved.
 //
 
+/*struct PhysicsCategory {
+    static let none : UInt32 = 0;
+    static let all : UInt32 = UInt32.max;
+    static let player : UInt32 = 0b10;
+    static let edge : UInt32 = 0b1;
+}*/
+
 import SpriteKit
 import GameplayKit
 import CoreMotion
@@ -14,17 +21,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let manager = CMMotionManager();
     var player = SKSpriteNode();
+    //var edge1 = SKSpriteNode();
     
+    let tiltsensitivity = 5.0;
     var prevTime:TimeInterval = 0
     let starttime = Date();
-    let planeRadiusStart = 300.0;
-    var planeRadius = 300;
+    let planeRadiusStart = 200.0;
+    var planeRadius = 200;
     var planeScale = 1.0;
-    let plane = SKShapeNode(circleOfRadius: 300);
-    let planeDx = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14);
-    let planeDy = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14);
-    
-    let playerRadius = 32;
+    let plane = SKShapeNode(circleOfRadius: 200);
+    let planeDx = Bool.random() ? Int.random(in: -5...(-3)) : Int.random(in: 3...5);//Guaranteed to be not zero
+    let planeDy = Bool.random() ? Int.random(in: -5...(-3)) : Int.random(in: 3...5);
+    var playerRadius = 64;
     var minimumGap = 5;
     
     override func didMove(to view: SKView) {
@@ -35,9 +43,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //Creating Objects
         let textr = SKTexture(imageNamed: "tempball2");
         player = SKSpriteNode(texture: textr);
-        player.setScale(0.25)
+        //player.position = CGPoint(x: frame.midX, y: 2*frame.midY);
+        player.setScale(0.5)
         player.position = CGPoint(x: 0, y: 0);
-
+        
+        /*let dimensionsedges = CGSize(width: 10, height: 10);
+        edge1 = SKSpriteNode(color: UIColor.red, size: dimensionsedges);
+        edge1.position = CGPoint(x: 0, y: -size.height/2);*/
+ 
         plane.name = "plane";
         plane.position = CGPoint(x: 0, y: 0);
         plane.fillColor = SKColor.green;
@@ -45,32 +58,56 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //Accelerometer/Gravity
         manager.startAccelerometerUpdates();
         manager.accelerometerUpdateInterval = 0.05;
-        if let tiltdata = manager.accelerometerData {
-            physicsWorld.gravity = CGVector(dx: tiltdata.acceleration.x * 15, dy: tiltdata.acceleration.y * 15);
-        }
-        self.physicsWorld.contactDelegate = self;
         
         //Physics
-        
+        self.physicsWorld.contactDelegate = self;
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2);
-        player.physicsBody!.affectedByGravity = false;
+        player.physicsBody!.affectedByGravity = true;
         //player.physicsBody?.categoryBitMask = PhysicsCategory.player;
         //player.physicsBody?.contactTestBitMask = PhysicsCategory.edge;//Collides with edges
         player.physicsBody?.collisionBitMask = 0;//Cannot bounce off of anything, this line must be in otherwise the ball freaks out
         //player.physicsBody?.usesPreciseCollisionDetection = true;
         
         //this is fake gravity because real gravity moves too fast when I test on an iMac
-        player.physicsBody!.velocity = CGVector(dx: planeDx, dy: planeDy);
+        //player.physicsBody!.velocity = CGVector(dx: 0, dy: 0);
+        
+        /*edge1.physicsBody = SKPhysicsBody(rectangleOf: edge1.size);
+        edge1.physicsBody?.isDynamic = false;
+        edge1.physicsBody?.categoryBitMask = PhysicsCategory.edge;
+        edge1.physicsBody?.contactTestBitMask = PhysicsCategory.edge;//Collides with edges
+        edge1.physicsBody?.collisionBitMask = PhysicsCategory.none;//Cannot bounce off of anything
+        edge1.physicsBody?.usesPreciseCollisionDetection = true;*/
  
         plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius));
-        plane.physicsBody!.affectedByGravity = false;
+        plane.physicsBody!.isDynamic = false;
         plane.physicsBody!.linearDamping = 0;
         plane.physicsBody!.mass = 0.0;
         plane.physicsBody!.velocity = CGVector(dx: planeDx, dy: planeDy);
         
         self.addChild(player);
+        //self.addChild(edge1);
         self.addChild(plane);
     }
+    
+    
+    /*//Function that is called when there is suspected contact
+    func didBegin(_ contact: SKPhysicsContact){
+        var body1: SKPhysicsBody//Sorted to be the player
+        var body2: SKPhysicsBody//Sorted to be an edge
+        if contact.bodyA.categoryBitMask > contact.bodyB.categoryBitMask {
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+        }
+        else {
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        if ((body1.categoryBitMask & PhysicsCategory.player != 0) && (body2.categoryBitMask & PhysicsCategory.edge != 0)) {
+            //we have contact, game over
+            /*player.removeFromParent();//Just to see if it works*/
+        }
+    }*/
+ 
 
     //Used to bounce the plane off of screen edges
     override func didSimulatePhysics() {
@@ -94,6 +131,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //Used to test detection
     override func update(_ currentTime: CFTimeInterval) {
         
+        if let tiltdata = manager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: -tiltdata.acceleration.y * tiltsensitivity, dy: tiltdata.acceleration.x * tiltsensitivity);//Swapped because of landscape
+        }
+        
         if currentTime - prevTime > (1.0/30.0){ //30 FPS gameplay
             prevTime = currentTime;
             
@@ -103,7 +144,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             if distanceR > CGFloat(planeRadius - playerRadius) {
                 //sometimes the ball and plane make a sudden jump at the end of the game
-                plane.physicsBody?.isDynamic = false;
+                //plane.physicsBody?.isDynamic = false;
                 player.physicsBody?.isDynamic = false;
                 print ("YOU LOSE");
                 print ("Time: ",-starttime.timeIntervalSinceNow);
@@ -113,11 +154,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if planeRadius > (playerRadius + minimumGap) {
                 let currentVelocity = CGVector(dx: plane.physicsBody!.velocity.dx, dy: plane.physicsBody!.velocity.dy);
                 // Any function you put here will execute every second
-                planeScale *= 0.999;
+                planeScale -= 0.0001;
                 planeRadius = Int(planeScale*planeRadiusStart);
                 plane.setScale(CGFloat(planeScale));
-                plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius));
-                plane.physicsBody!.affectedByGravity = false;
+                plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius));                plane.physicsBody!.affectedByGravity = false;
                 plane.physicsBody!.linearDamping = 0;
                 plane.physicsBody!.mass = 0.0;
                 plane.physicsBody!.velocity = currentVelocity;
@@ -126,7 +166,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             //print ("x: ", player.anchorPoint.x, " | ", distanceX, " | ", plane.position.x);
             //print ("y: ", player.anchorPoint.y, " | ", distanceY, " | ", plane.position.y);
-            //print ("r: ", distanceX, " | ", distanceY, " | ", distanceR);
+            print ("r: ", distanceX, " | ", distanceY, " | ", distanceR);
         }
     }
  
