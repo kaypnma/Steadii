305ced0fa568e2968afa6daf53600e5b48fdf384
Kay Arellano <karellan@lib3000-icmac27.ucslabs.sfu.ca>
2019-11-01 17:19:10 -0700

    reupdated ball game after reverts

--- steadii/steadii/Ballgame/GameScene.swift
+++ steadii/steadii/Ballgame/GameScene.swift
@@ -5,20 +5,20 @@
 //  Implementation of Ball Tilt Game
 //  Created by ckeilbar on 10/23/19
 //  Last Updated by /*Name(s)*/ on /*Date*/
-/*  
-    - Collision is correctly detected
-    - 
-*/
+/*
+ - Collision is correctly detected
+ -
+ */
 
 //  Known Bugs:
 /*
-    - Timer still goes on even after the player has lost
-*/
+ - Timer still goes on even after the player has lost
+ */
 
 //  To do:
 /*
-    - Test Gravity
-*/
+ - Test Gravity
+ */
 
 //  Copyright © 2019 ii Studio. All rights reserved.
 
@@ -35,29 +35,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let tiltsensitivity = 4.0;
     var prevTime:TimeInterval = 0
     let starttime = Date();
-    let planeRadiusStart = 150.0;
-    var planeRadius = 150;
+    let planeRadiusStart = 300.0;
+    var planeRadius = 300;
     var planeScale = 1.0;
-    let plane = SKShapeNode(circleOfRadius: 150);
+    let plane = SKShapeNode(circleOfRadius: 300);
     let planeDx = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14);//Guaranteed to be not zero
     let planeDy = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14);
     var playerRadius = 32;
     var minimumGap = 5;
-    	
+    
     override func didMove(to view: SKView) {
         
         //actually center the scene to have the origin at the centre of the screen
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5);
-        
+        backgroundColor = UIColor(red: 250/255, green: 207/255, blue: 142/255, alpha: 1);
         //Creating Objects
         let textr = SKTexture(imageNamed: "tempball2");
         player = SKSpriteNode(texture: textr);
         player.setScale(0.25)
         player.position = CGPoint(x: 0, y: 0);
- 
+        
         plane.name = "plane";
         plane.position = CGPoint(x: 0, y: 0);
-        plane.fillColor = SKColor.green;
+        plane.fillColor = UIColor(red: 210/255, green: 231/255, blue: 230/255, alpha: 1);
         
         //Accelerometer/Gravity
         manager.startAccelerometerUpdates();
@@ -71,7 +71,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //this is fake gravity because real gravity moves too fast when I test on an iMac
         //player.physicsBody!.velocity = CGVector(dx: 0, dy: 0);
- 
+        
         plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius));
         plane.physicsBody!.affectedByGravity = false;
         plane.physicsBody!.linearDamping = 0;
@@ -82,25 +82,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(player);
         self.addChild(plane);
     }
-
+    
     //Used to bounce the plane off of screen edges
     override func didSimulatePhysics() {
         let plane = childNode(withName: "plane")!
         
         if (plane.position.x > (size.width/2.0 - CGFloat(planeRadius)) &&
             plane.physicsBody!.velocity.dx > 0) ||
-           (plane.position.x < (-size.width/2.0 + CGFloat(planeRadius)) &&
-            plane.physicsBody!.velocity.dx < 0) {
+            (plane.position.x < (-size.width/2.0 + CGFloat(planeRadius)) &&
+                plane.physicsBody!.velocity.dx < 0) {
             plane.physicsBody!.velocity.dx *= -1;
         }
-
+        
         if (plane.position.y > (size.height/2.0 - CGFloat(planeRadius)) && plane.physicsBody!.velocity.dy > 0) ||
-           (plane.position.y < (-size.height/2.0 + CGFloat(planeRadius)) &&
-            plane.physicsBody!.velocity.dy < 0) {
+            (plane.position.y < (-size.height/2.0 + CGFloat(planeRadius)) &&
+                plane.physicsBody!.velocity.dy < 0) {
             plane.physicsBody!.velocity.dy *= -1;
         }
     }
- 
+    
     //Used to monitor gravity
     //Used to change the size of the plane
     //Used to test detection
@@ -150,6 +150,5 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             print ("lose distance: ", CGFloat(planeRadius - playerRadius));
         }
     }
- 
+    
 }
-
