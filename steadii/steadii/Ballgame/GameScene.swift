018670e57eb754b99dad80408ace1a7d48b334d3
zzqu <zzqu@sfu.ca>
2019-11-03 14:52:17 -0800

    made ball game more modular, created unit tests for ball game

--- steadii/steadii/Ballgame/GameScene.swift
+++ steadii/steadii/Ballgame/GameScene.swift
@@ -20,7 +20,7 @@
 
 //  To do:
 /*
- - Test Gravity
+    None
 */
 
 //  Copyright © 2019 ii Studio. All rights reserved.
@@ -38,35 +38,68 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let manager = CMMotionManager()
     var player = SKSpriteNode()
-    //var edge1 = SKSpriteNode()
     var GameOver:SKLabelNode!
     
-    let tiltSensitivity = 4.0
     var prevTime:TimeInterval = 0
     let startTime = Date()
-    let planeRadiusStart = 300.0
-    var planeRadius = 300
-    var planeScale = 1.0
-    let plane = SKShapeNode(circleOfRadius: 300)
-    let planeDx = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14)//Guaranteed to be not zero
-    let planeDy = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14)
-    var playerRadius = 32
-    var minimumGap = 5
+    
+    var tiltSensitivity:Double
+    
+    var planeRadiusStart:Double
+    var planeRadius:Double
+    var planeScale:Double
+    
+    var plane:SKShapeNode
+    var planeDx:Int
+    var planeDy:Int
+    
+    var playerRadius:Double
+    var minimumGap:Double
+    
+    init (  size: CGSize,
+            PtiltSensitivity: Double = 4.0,
+            planeRadiusStart: Double = 300.0,
+            planeScale: Double = 1.0,
+            planeDxDyRandom: Bool = true,
+            planeDxDy: CGVector = CGVector(dx:10, dy:5),
+            playerRadius: Double = 32.0,
+            PminimumGap: Double = 5.0) {
+        tiltSensitivity = PtiltSensitivity
+        
+        self.planeRadiusStart = planeRadiusStart
+        planeRadius = planeRadiusStart
+        self.planeScale = planeScale
+        
+        plane = SKShapeNode(circleOfRadius: CGFloat(planeRadiusStart))
+        
+        if planeDxDyRandom {
+            planeDx = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14)//Guaranteed to be not zero
+            planeDy = Bool.random() ? 3*Int.random(in: -14...(-8)) : 3*Int.random(in: 8...14)
+        }
+        else {
+            planeDx = Int(planeDxDy.dx)
+            planeDy = Int(planeDxDy.dy)
+        }
+        
+        self.playerRadius = playerRadius
+        minimumGap = PminimumGap
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
         
         //actually center the scene to have the origin at the centre of the screen
-        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
         backgroundColor = UIColor(red: 250/255, green: 207/255, blue: 142/255, alpha: 1)
         //Creating Objects
-        let textr = SKTexture(imageNamed: "tempball2")
-        player = SKSpriteNode(texture: textr)
-        player.setScale(0.25)
-        player.position = CGPoint(x: 0, y: 0)
         
-        plane.name = "plane"
-        plane.position = CGPoint(x: 0, y: 0)
-        plane.fillColor = UIColor(red: 210/255, green: 231/255, blue: 230/255, alpha: 1)
+        initializePlayer()
+        initializePlane(planePosition: CGPoint(x: 0, y: 0))
         
         //Accelerometer/Gravity
         manager.startAccelerometerUpdates()
@@ -74,19 +107,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //Physics
         self.physicsWorld.contactDelegate = self
-        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
-        player.physicsBody!.affectedByGravity = true
-        player.physicsBody?.collisionBitMask = 0//Cannot bounce off of anything, this line must be in otherwise the ball freaks out
-        
-        //this is fake gravity because real gravity moves too fast when I test on an iMac
-        //player.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
-        
-        plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius))
-        plane.physicsBody!.affectedByGravity = false
-        plane.physicsBody!.linearDamping = 0
-        plane.physicsBody!.mass = 0.0
-        plane.physicsBody!.velocity = CGVector(dx: planeDx, dy: planeDy)
-        plane.physicsBody?.collisionBitMask = 0
+        initializePlanePhysics(planePhysicsVelocity: CGVector(dx: planeDx, dy: planeDy))
         
         self.addChild(player)
         self.addChild(plane)
@@ -96,6 +117,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didSimulatePhysics() {
         let plane = childNode(withName: "plane")!
         
+        checkEdgeCollision(plane: plane);
+    }
+    
+    //Used to monitor gravity
+    //Used to change the size of the plane
+    //Used to test detection
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        
+        if let tiltdata = manager.accelerometerData {
+            updateTileData(tiltdata: tiltdata)
+        }
+        
+        if currentTime - prevTime > (1.0/30.0){ //30 FPS gameplay
+            prevTime = currentTime
+            
+            let distanceX = player.position.x - plane.position.x
+            let distanceY = player.position.y - plane.position.y
+            let distanceR = sqrt(pow(distanceX,2) + pow(distanceY,2))
+            
+            //Detect game over condition
+            if  gameOver(distanceR: distanceR) {
+                return
+            }
+            
+            //Normal gameplay difficulty increase via plane radius decrease
+            
+            updatePlaneSize()
+            
+            //print ("x: ", player.anchorPoint.x, " | ", distanceX, " | ", plane.position.x)
+            //print ("y: ", player.anchorPoint.y, " | ", distanceY, " | ", plane.position.y)
+            //print ("r: ", distanceX, " | ", distanceY, " | ", distanceR)
+        }
+    }
+    
+    func checkEdgeCollision (plane: SKNode){
         if (plane.position.x > (size.width/2.0 - CGFloat(planeRadius)) &&
             plane.physicsBody!.velocity.dx > 0) ||
             (plane.position.x < (-size.width/2.0 + CGFloat(planeRadius)) &&
@@ -110,67 +167,77 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
-    //Used to monitor gravity
-    //Used to change the size of the plane
-    //Used to test detection
+    func updateTileData (tiltdata: CMAccelerometerData) {
+        physicsWorld.gravity = CGVector(dx: -tiltdata.acceleration.y * tiltSensitivity, dy: tiltdata.acceleration.x * tiltSensitivity)//Swaped because of landscape
+    }
     
-    override func update(_ currentTime: CFTimeInterval) {
-        
-        if let tiltdata = manager.accelerometerData {
-            physicsWorld.gravity = CGVector(dx: -tiltdata.acceleration.y * tiltSensitivity, dy: tiltdata.acceleration.x * tiltSensitivity)//Swapped because of landscape
-        }
-        
-        if currentTime - prevTime > (1.0/30.0){ //30 FPS gameplay
-            prevTime = currentTime
+    func gameOver (distanceR: CGFloat) -> Bool {
+        if distanceR > CGFloat(planeRadius - playerRadius) {
+            plane.physicsBody?.isDynamic = false
+            player.physicsBody?.isDynamic = false
+            //print ("YOU LOSE")
+            //print ("Time: ", Double(round(1000*(-startTime.timeIntervalSinceNow))/1000))
+            self.isPaused = true
             
-            let distanceX = player.position.x - plane.position.x
-            let distanceY = player.position.y - plane.position.y
-            let distanceR = sqrt(pow(distanceX,2) + pow(distanceY,2))
+            //Outputs a game over sign when the game ends
+            GameOver = SKLabelNode(text: "GAME OVER\nDURATION: " + String(Double(round(1000*(-startTime.timeIntervalSinceNow))/1000)) + " seconds")
+            GameOver.numberOfLines = 0
+            GameOver.fontSize = 100
+            GameOver.fontName = "Corbel-Bold"
+            GameOver.fontColor = UIColor.black
             
-            //Detect game over condition
-            if distanceR > CGFloat(planeRadius - playerRadius) {
-                //sometimes the ball and plane make a sudden jump at the end of the game
-                plane.physicsBody?.isDynamic = false
-                player.physicsBody?.isDynamic = false
-                print ("YOU LOSE")
-                print ("Time: ", Double(round(1000*(-startTime.timeIntervalSinceNow))/1000))
-                self.isPaused = true
-                
-                //Outputs a game over sign when the game ends
-                GameOver = SKLabelNode(text: "GAME OVER")
-                GameOver.fontSize = 100
-                GameOver.fontName = "Corbel-Bold"
-                GameOver.fontColor = UIColor.black
-                
-                self.addChild(GameOver)
-                
-                //notification for the exit button to appear when the game ends
+            self.addChild(GameOver)
+            
+            //notification for the exit button to apear when the game ends
             NotificationCenter.default.post(name:NSNotification.Name("ExitButtonNotification"), object:nil)
-                
-                return
-            }
-            
-            //Normal gameplay difficulty increase via plane radius decrease
-            if planeRadius > (playerRadius + minimumGap) {
-                let currentVelocity = CGVector(dx: plane.physicsBody!.velocity.dx, dy: plane.physicsBody!.velocity.dy)
-                // Any function you put here will execute every second
-                planeScale *= 0.999
-                planeRadius = Int(planeScale*planeRadiusStart)
-                plane.setScale(CGFloat(planeScale))
-                plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius))
-                plane.physicsBody!.affectedByGravity = false
-                plane.physicsBody!.linearDamping = 0
-                plane.physicsBody!.mass = 0.0
-                plane.physicsBody!.velocity = currentVelocity
-                plane.physicsBody!.collisionBitMask = 0
-                //print("size:", planeRadius)
-            }
-            
-            //print ("x: ", player.anchorPoint.x, " | ", distanceX, " | ", plane.position.x)
-            //print ("y: ", player.anchorPoint.y, " | ", distanceY, " | ", plane.position.y)
-            print ("r: ", distanceX, " | ", distanceY, " | ", distanceR)
-            print ("lose distance: ", CGFloat(planeRadius - playerRadius))
+        
+            return true
+        }
+        else {
+            return false
         }
     }
     
+    func updatePlaneSize() {
+        if planeRadius > (playerRadius + minimumGap) {
+            let currentVelocity = CGVector(dx: plane.physicsBody!.velocity.dx, dy: plane.physicsBody!.velocity.dy)
+            planeScale *= 0.999
+            planeRadius = planeScale*planeRadiusStart
+            plane.setScale(CGFloat(planeScale))
+            plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius))
+            plane.physicsBody!.affectedByGravity = false
+            plane.physicsBody!.linearDamping = 0
+            plane.physicsBody!.mass = 0.0
+            plane.physicsBody!.velocity = currentVelocity
+            plane.physicsBody!.collisionBitMask = 0
+        }
+    }
+    
+    func initializePlayer(textr: SKTexture = SKTexture(imageNamed: "tempball2")) {
+        player = SKSpriteNode(texture: textr)
+        player.setScale(0.25)
+        player.position = CGPoint(x: 0, y: 0)
+    }
+    
+    func initializePlane(planePosition: CGPoint = CGPoint(x:0, y:0)) {
+        plane.name = "plane"
+        plane.position = planePosition
+        plane.fillColor = UIColor(red: 210/255, green: 231/255, blue: 230/255, alpha: 1)
+    }
+    
+    func initializePlanePhysics(planePhysicsVelocity: CGVector = CGVector(dx: 0, dy: 0)) {
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody!.affectedByGravity = true
+        player.physicsBody?.collisionBitMask = 0//Cannot bounce off of anything, this line must be in otherwise the ball freaks out
+        
+        //this is fake gravity because real gravity moves too fast when I test on an iMac
+        //player.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
+        
+        plane.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(planeRadius))
+        plane.physicsBody!.affectedByGravity = false
+        plane.physicsBody!.linearDamping = 0
+        plane.physicsBody!.mass = 0.0
+        plane.physicsBody!.velocity = planePhysicsVelocity
+        plane.physicsBody?.collisionBitMask = 0
+    }
 }
--- steadii/steadiiTests/steadiiTests.swift
+++ steadii/steadiiTests/steadiiTests.swift
@@ -26,23 +26,81 @@
 //  Copyright © 2019 ii Studio. All rights reserved.
 
 import XCTest
+import SpriteKit
 @testable import steadii
 
 class steadiiTests: XCTestCase {
 
+    var sut: GameScene!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = GameScene(size: CGSize(width: 1000, height: 1000),
+                        planeDxDyRandom: false)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCheckEdgeCollision() {
+        // Given
+        let initialVelocity = CGVector(dx: sut.planeDx, dy: sut.planeDy)
+        sut.initializePlane(planePosition: CGPoint(x: 1000, y: 1000))
+        sut.initializePlanePhysics(planePhysicsVelocity: initialVelocity)
+        
+        // When
+        sut.checkEdgeCollision(plane: sut.plane)
+        let resultDx = sut.plane.physicsBody!.velocity.dx.rounded()
+        let checkDx = (initialVelocity.dx == -resultDx)
+        let resultDy = sut.plane.physicsBody!.velocity.dy.rounded()
+        let checkDy = (initialVelocity.dy == -resultDy)
+        
+        // Then
+        print("checkDx: ", checkDx, " : ", initialVelocity.dx, "==", -resultDx)
+        print("checkDy: ", checkDy, " : ", initialVelocity.dy, "==", -resultDy)
+        XCTAssert(checkDx && checkDy)
     }
-
+    
+    func testGameOver() {
+        // Given
+        sut.initializePlayer()
+        sut.initializePlane(planePosition: CGPoint(x: 1000, y: 1000))
+        let distanceX = sut.player.position.x - sut.plane.position.x
+        let distanceY = sut.player.position.y - sut.plane.position.y
+        let distanceR = sqrt(pow(distanceX,2) + pow(distanceY,2))
+        
+        // When
+        let checkGameOver = ((sut.gameOver(distanceR: distanceR)) &&
+                            (sut.isPaused == true) &&
+                            !(sut?.GameOver == nil))
+        
+        // Then
+        print("checkGameOver: ", checkGameOver, " : ", distanceR)
+        XCTAssert(checkGameOver)
+    }
+    
+    func testUpdatePlaneSize() {
+        // Given
+        sut.initializePlayer()
+        sut.initializePlane()
+        sut.initializePlanePhysics()
+        let currentPlaneScale = sut.planeScale
+        
+        // When
+        sut.updatePlaneSize()
+        let checkPlaneScale = (sut.planeScale == currentPlaneScale * 0.999)
+        let checkPlaneRadius = (sut.planeRadius == currentPlaneScale * 0.999 * sut.planeRadiusStart)
+        
+        // Then
+        print("checkPlaneScale: ", checkPlaneScale, " : ", sut.planeScale, "==", currentPlaneScale * 0.999)
+        print("checkPlaneRadius: ", checkPlaneRadius, " : ", sut.planeRadius, "==", currentPlaneScale * 0.999 * sut.planeRadiusStart)
+        XCTAssert(checkPlaneScale && checkPlaneRadius)
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
