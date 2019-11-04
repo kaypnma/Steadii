0b04f4ec480834984e707c5932e9494708901ee5
ckeilbar <chris_keilbart@sfu.ca>
2019-11-03 19:57:18 -0800

    Update CreateViewController.swift again, forgot date

--- steadii/steadii/CreateViewController.swift
+++ steadii/steadii/CreateViewController.swift
@@ -6,7 +6,7 @@
 //  Description/Purpose: Creates UI controller
 
 //  Created by Denyse Tran on 10/28/19
-//  Last Updated by Dustin Seah on 11/01/2019
+//  Last Updated by Chris Keilbart on 11/03/2019
 //  Worked on by Denyse Trans, Jack Guo, Dustin Seah, and Chris Keilbart
 
 //  Updates from Previous Commit:
