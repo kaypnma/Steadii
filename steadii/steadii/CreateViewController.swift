f54d9180068cfbb9755d77ed43c19c208f7f8b7b
ckeilbar <chris_keilbart@sfu.ca>
2019-11-03 19:47:06 -0800

    Update CreateViewController.swift with newer header

--- steadii/steadii/CreateViewController.swift
+++ steadii/steadii/CreateViewController.swift
@@ -7,6 +7,7 @@
 
 //  Created by Denyse Tran on 10/28/19
 //  Last Updated by Dustin Seah on 11/01/2019
+//  Worked on by Denyse Trans, Jack Guo, Dustin Seah, and Chris Keilbart
 
 //  Updates from Previous Commit:
 /*
