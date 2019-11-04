efe9375df98416d2478e4cc3e10f46360e1fad64
ckeilbar <chris_keilbart@sfu.ca>
2019-11-03 19:54:25 -0800

    Update TextField.swift

--- steadii/steadii/CustomView/TextField.swift
+++ steadii/steadii/CustomView/TextField.swift
@@ -7,6 +7,7 @@
 
 //  Created by Kay Arellano on 10/27/19
 //  Last Updated by Dustin Seah on 11/01/2019
+//  Worked on by Kay Arellano, Dustin Seah, and Chris Keilbart
 
 //  Updates from Previous Commit:
 /*  
