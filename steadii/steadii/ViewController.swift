c8111de97158c1e3c108446412400531606cf877
ckeilbar <chris_keilbart@sfu.ca>
2019-11-03 19:56:06 -0800

    Update ViewController.swift again, forgot the date

--- steadii/steadii/ViewController.swift
+++ steadii/steadii/ViewController.swift
@@ -6,7 +6,7 @@
 //  Description/Purpose: defines view controller
 
 //  Created by Kay Arellano on 10/26/19
-//  Last Updated by Dustin Seah on 11/01/2019
+//  Last Updated by Chris Keilbart on 11/03/2019
 //  Worked on by Kay Arellano, Dustin Seah, Jack Guo, Denyse Tran, and Chris Keilbart
 
 //  Updates from Previous Commit:
